825ab4f516c039bba76883387177bb4d67bad49b
Ely Apao <jozzua@gmail.com>
2013-08-27 16:20:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mygarage::Application.config.secret_key_base = '012a9f13dd6d29555d7601e954cfe38bf0c603613b83df730a49fb9980d6bfdea34bffd7ba9627c3c4d47ad35fd2b5fdcbec42709dedaab8b50e5e44e7921c65'
