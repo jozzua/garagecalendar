1871f4c4b851a986a54780d7404c0c3c0fbe9c62
Ely Apao <jozzua@gmail.com>
2013-08-27 16:29:52 +0800

    Add Foundation Zurb styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Mygarage::Application.routes.draw do
+  resources :vehicles
+  root to: 'vehicles#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
